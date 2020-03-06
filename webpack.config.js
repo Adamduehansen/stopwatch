e58f22d3c45ee899f86752f0636e32468853d842
Adam Due Hansen <adamduehan@gmail.com>
2020-03-07 00:25:58 +0100

    Adds Webpack build.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src", "index.ts"),
+  module: {
+    rules: [
+      {
+        test: /\.ts?/,
+        use: "ts-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".ts"]
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "stopwatch.js"
+  }
+}
